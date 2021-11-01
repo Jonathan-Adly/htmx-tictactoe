e4df2d5c55ed4c69ac3e9d40760a6d03786a5515
Jonathan Adly <jonathanadly@Jonathans-MacBook-Air.local>
2021-11-01 13:55:38 -0400

    login required

--- tictactoe/views.py
+++ tictactoe/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     return render(request, "home.html")
 
 
+@login_required
 def change_player(request):
     if request.user.player == "X":
         request.user.player = "O"
