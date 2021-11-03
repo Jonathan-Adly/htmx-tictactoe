55cd87515a9297dc9d79aa96cbdae2be6a4f6b9f
Jonathan Adly <jonathanadly@Jonathans-MacBook-Air.local>
2021-11-03 16:14:34 -0400

    done

--- tictactoe/utils.py
+++ tictactoe/utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def computer_move(user):
     board = user.board
     if user.player == "X":
@@ -16,9 +19,9 @@ def computer_move(user):
 
 
 def check_status(board):
-    # if check_winner(board):
-    # status= "--- have won the game"
-    # return and terminate here
+    if check_winner(board) != 0:
+        status = f"The {check_winner(board)} player have won the game"
+        return status
     flat_list = []
     for row in board:
         for col in row:
@@ -32,3 +35,42 @@ def check_status(board):
 
     else:
         return None
+
+
+def check_rows(board):
+    for row in board:
+        if len(set(row)) == 1:
+            if row[0] == 1:
+                return "X"
+            elif row[0] == 2:
+                return "O"
+            else:
+                return row[0]
+    return 0
+
+
+def check_diagonals(board):
+    if len(set([board[i][i] for i in range(len(board))])) == 1:
+        if board[0][0] == 1:
+            return "X"
+        elif board[0][0] == 2:
+            return "O"
+        else:
+            return board[0][0]
+    if len(set([board[i][len(board) - i - 1] for i in range(len(board))])) == 1:
+        if board[0][len(board) - 1] == 1:
+            return "X"
+        elif board[0][len(board) - 1] == 2:
+            return "O"
+        else:
+            return board[0][len(board) - 1]
+    return 0
+
+
+def check_winner(board):
+    # transposition to check rows, then columns
+    for new_board in [board, np.transpose(board)]:
+        result = check_rows(new_board)
+        if result:
+            return result
+    return check_diagonals(board)
