b7e32e17d39e56abbf4cf07beeffbc6f630e5179
Jonathan Adly <jonathanadly@jonathans-air.myfiosgateway.com>
2021-11-02 20:29:39 -0400

    complete

--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("change-player", views.change_player, name="change_player"),
+    path("play", views.play, name="play"),
+    path("reset", views.reset, name="reset"),
 ]
--- tictactoe/views.py
+++ tictactoe/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from accounts.models import CustomUser
 
 
 def home(request):
@@ -15,3 +17,25 @@ def change_player(request):
 
     request.user.save()
     return render(request, "components/game_info.html")
+
+
+@require_POST
+@login_required
+def play(request):
+    if request.user.player == "X":
+        player = 1
+    else:
+        player = 2
+    row = int(request.POST["row"])
+    col = int(request.POST["col"])
+    request.user.board[row][col] = player
+    request.user.save()
+    return render(request, "components/square.html")
+
+
+@login_required
+def reset(request):
+    request.user.board = CustomUser.get_default()
+    request.user.player = "X"
+    request.user.save()
+    return redirect("home")
